2581efd96b5b57c4ddc69993e25bbaf97b565737
Lucas Amado <lucasamado@Lucass-MacBook-Air.local>
2023-10-23 18:29:05 +0100

    Initial commit

---
+++ src/main/java/pt/amado/wisetax/WiseTaxApplication.java
@@ -0,0 +1,13 @@
+package pt.amado.wisetax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseTaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseTaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/amado/wisetax/WiseTaxApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amado.wisetax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseTaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
