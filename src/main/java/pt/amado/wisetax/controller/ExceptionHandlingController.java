cd79577f3fe01ddff17f5200e163ca9308b39978
Lucas Amado <syst.lucasamado@gmail.com>
2023-10-23 20:09:03 +0100

    Defining an approach to handle with errors thrown by the application

--- src/main/java/pt/amado/wisetax/controller/ExceptionHandlingController.java
+++ src/main/java/pt/amado/wisetax/controller/ExceptionHandlingController.java
@@ -1,2 +1,36 @@
-package pt.amado.wisetax.controller;public class ExceptionHandlingController {
+package pt.amado.wisetax.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pt.amado.wisetax.dto.ExceptionMessageDTO;
+
+@ControllerAdvice
+public class ExceptionHandlingController {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<ExceptionMessageDTO> handleException(Exception ex) {
+        ExceptionMessageDTO exceptionDTO = ExceptionMessageDTO.builder()
+                .code(500)
+                .generalMessage("An error occurred while processing the request.")
+                .specificMessage(ex.getMessage()).build();
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionMessageDTO> handleValidationException(MethodArgumentNotValidException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionMessageDTO exceptionDTO = ExceptionMessageDTO.builder()
+                .code(status.value())
+                .generalMessage("An error occurred while validation of provided input.")
+                .specificMessage(ex.getMessage()).build();
+
+        return ResponseEntity.status(status).body(exceptionDTO);
+    }
+
 }
