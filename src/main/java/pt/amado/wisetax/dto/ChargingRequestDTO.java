40256dbadb31858e09f915177e8e42a8b8c0aeb6
Lucas Amado <syst.lucasamado@gmail.com>
2023-10-23 20:07:51 +0100

    Adding dtos

--- src/main/java/pt/amado/wisetax/dto/ChargingRequestDTO.java
+++ src/main/java/pt/amado/wisetax/dto/ChargingRequestDTO.java
@@ -1,2 +1,22 @@
-package pt.amado.wisetax.dto;public class CharginRequestDTO {
+package pt.amado.wisetax.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class ChargingRequestDTO {
+    @NotEmpty(message = "Service is required")
+    @Pattern(regexp = "^[AB]$", message = "Service must be 'A' or 'B'")
+    private String service;
+
+    private boolean roaming;
+
+    @Pattern(regexp = "\\+?[1-9]\\d{1,14}", message = "Invalid MSISDN format")
+    private String msisdn;
+
+    @Positive(message = "RSU must be a positive number")
+    private int rsu;
+
 }
--- src/main/java/pt/amado/wisetax/dto/ExceptionMessageDTO.java
+++ src/main/java/pt/amado/wisetax/dto/ExceptionMessageDTO.java
@@ -1,2 +1,12 @@
-package pt.amado.wisetax.dto;public class ExceptionMessageDTO {
+package pt.amado.wisetax.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ExceptionMessageDTO {
+    private int code;
+    private String generalMessage;
+    private String specificMessage;
 }
