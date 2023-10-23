f92248b116ecd7c1b1be6e0197b1ef0c7189b215
Lucas Amado <syst.lucasamado@gmail.com>
2023-10-23 20:08:33 +0100

    The main class used to handle with requests

--- src/main/java/pt/amado/wisetax/controller/RequestController.java
+++ src/main/java/pt/amado/wisetax/controller/RequestController.java
@@ -1,2 +1,20 @@
-package pt.amado.wisetax.controller;public class RequestController {
+package pt.amado.wisetax.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pt.amado.wisetax.dto.ChargingRequestDTO;
+
+@Controller
+@RequestMapping("api/v1")
+public class RequestController {
+
+    @PostMapping
+    ResponseEntity<String> processRequest(@RequestBody @Validated ChargingRequestDTO dto) {
+        return ResponseEntity.ok("OK");
+    }
+
 }
